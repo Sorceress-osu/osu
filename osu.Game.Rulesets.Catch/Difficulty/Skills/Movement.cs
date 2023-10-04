e8ed8fbf13e6a2f0205038809c63d5f8c21d5c51
Sorceress-osu <sorceress4628@gmail.com>
2023-10-04 03:39:11 +0200

    Initial commit

--- osu.Game.Rulesets.Catch/Difficulty/Skills/Movement.cs
+++ osu.Game.Rulesets.Catch/Difficulty/Skills/Movement.cs
@@ -13,7 +13,14 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
     {
         private const float absolute_player_positioning_error = 16f;
         private const float normalized_hitobject_radius = 41.0f;
+
         private const double direction_change_bonus = 21.0;
+        private const double base_distance_bonus = 12.5;
+
+        private const double edge_dash_threshold = 20.0;
+        private const double edge_dash_bonus = 5.7;
+
+        private const double strain_time_weight = 16;
 
         protected override double SkillMultiplier => 900;
         protected override double StrainDecayBase => 0.2;
@@ -23,6 +30,10 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
         protected override int SectionLength => 750;
 
         protected readonly float HalfCatcherWidth;
+
+        private bool isDirectionChange;
+        private bool lastIsDirectionChange;
+        private bool lastHyperDash;
 
         private float? lastPlayerPosition;
         private float lastDistanceMoved;
@@ -59,47 +70,51 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
 
             float distanceMoved = playerPosition - lastPlayerPosition.Value;
 
-            double weightedStrainTime = catchCurrent.StrainTime + 13 + (3 / catcherSpeedMultiplier);
+            double weightedStrainTime = catchCurrent.StrainTime + (strain_time_weight / catcherSpeedMultiplier);
+            double lastWeightedStrainTime = lastStrainTime + (strain_time_weight / catcherSpeedMultiplier);
 
-            double distanceAddition = (Math.Pow(Math.Abs(distanceMoved), 1.3) / 510);
-            double sqrtStrain = Math.Sqrt(weightedStrainTime);
+            double weightedSqrtStrain = Math.Sqrt(weightedStrainTime);
+            double lastWeightedSqrtStrain = Math.Sqrt(lastWeightedStrainTime);
 
-            double edgeDashBonus = 0;
+            // Initial addition based on distance moved
+            double movementAddition = (Math.Pow(Math.Abs(distanceMoved), 1.3) / 510);
 
-            // Direction change bonus.
+            // Are we changing direction
+            isDirectionChange = (Math.Abs(distanceMoved) > 0.1 && Math.Abs(lastDistanceMoved) > 0.1 && Math.Sign(distanceMoved) != Math.Sign(lastDistanceMoved));
+
+            // Direction change bonus considering both current and previous movement
+            if (isDirectionChange)
+            {
+                double bonusFactor = Math.Min(50, Math.Abs(distanceMoved)) / 50;
+                double antiflowFactor = Math.Max(Math.Min(70, Math.Abs(lastDistanceMoved)) / 70, 0.38);
+
+                movementAddition += direction_change_bonus / lastWeightedSqrtStrain * bonusFactor * antiflowFactor * Math.Max(1 - Math.Pow(weightedStrainTime / 1000, 3), 0);
+            }
+
+            // Base bonus for every movement, giving some weight to streams
             if (Math.Abs(distanceMoved) > 0.1)
-            {
-                if (Math.Abs(lastDistanceMoved) > 0.1 && Math.Sign(distanceMoved) != Math.Sign(lastDistanceMoved))
-                {
-                    double bonusFactor = Math.Min(50, Math.Abs(distanceMoved)) / 50;
-                    double antiflowFactor = Math.Max(Math.Min(70, Math.Abs(lastDistanceMoved)) / 70, 0.38);
+                movementAddition += base_distance_bonus * Math.Min(Math.Abs(distanceMoved), normalized_hitobject_radius * 2) / (normalized_hitobject_radius * 6) / weightedSqrtStrain;
 
-                    distanceAddition += direction_change_bonus / Math.Sqrt(lastStrainTime + 16) * bonusFactor * antiflowFactor * Math.Max(1 - Math.Pow(weightedStrainTime / 1000, 3), 0);
-                }
+            // Bonus for edge dashes, bonus is scaled with strain time (pre clockrate adjustments) as edge dashes are easier at lower ms values
+            if (catchCurrent.LastObject.DistanceToHyperDash <= edge_dash_threshold && !catchCurrent.LastObject.HyperDash)
+                movementAddition *= 1.0 + edge_dash_bonus * ((edge_dash_threshold - catchCurrent.LastObject.DistanceToHyperDash) / edge_dash_threshold) * Math.Pow((Math.Min(catchCurrent.StrainTime * catcherSpeedMultiplier, 265) / 265), 1.5);
 
-                // Base bonus for every movement, giving some weight to streams.
-                distanceAddition += 12.5 * Math.Min(Math.Abs(distanceMoved), normalized_hitobject_radius * 2) / (normalized_hitobject_radius * 6) / sqrtStrain;
-            }
+            // After a hyperdash the player is at the exact position of the next fruit
+            if (catchCurrent.LastObject.HyperDash)
+                playerPosition = catchCurrent.NormalizedPosition;
 
-            // Bonus for edge dashes.
-            if (catchCurrent.LastObject.DistanceToHyperDash <= 20.0f)
-            {
-                if (!catchCurrent.LastObject.HyperDash)
-                    edgeDashBonus += 5.7;
-                else
-                {
-                    // After a hyperdash we ARE in the correct position. Always!
-                    playerPosition = catchCurrent.NormalizedPosition;
-                }
+            // Consecutive hyperdashes in the same direction require no change in input so are heavily nerfed
+            if (catchCurrent.LastObject.HyperDash && lastHyperDash && !isDirectionChange)
+                movementAddition *= 0.2;
 
-                distanceAddition *= 1.0 + edgeDashBonus * ((20 - catchCurrent.LastObject.DistanceToHyperDash) / 20) * Math.Pow((Math.Min(catchCurrent.StrainTime * catcherSpeedMultiplier, 265) / 265), 1.5); // Edge Dashes are easier at lower ms values
-            }
 
             lastPlayerPosition = playerPosition;
             lastDistanceMoved = distanceMoved;
             lastStrainTime = catchCurrent.StrainTime;
+            lastIsDirectionChange = isDirectionChange;
+            lastHyperDash = catchCurrent.LastObject.HyperDash;
 
-            return distanceAddition / weightedStrainTime;
+            return movementAddition / weightedStrainTime;
         }
     }
 }
